cbd08cf1dae6686cce25df8dd54db10c3540347a
ha-da-ra <gold85600@gmail.com>
2024-05-08 22:57:56 +0200

    switch to ts

---
+++ express_server/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import * as middlewares from './middlewares';
+import router from './router';
+import MessageResponse from './interfaces/MessageResponse';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  });
+});
+
+app.use('/api/v1', router);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+export default app;
