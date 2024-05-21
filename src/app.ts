d857f5035774503f11791ee7aa62be2924373ec9
ha-da-ra <gold85600@gmail.com>
2024-05-21 16:18:18 +0200

    add styling to error page

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 import { getAllOwners } from './database/owners/owner-crud';
 import { ownerRouter } from './router/ownerRouter';
-import {urlencoded} from "body-parser";
+import { urlencoded } from "body-parser";
 import { viewRouter } from './router/viewRouter';
 import { NextFunction, Request, Response } from "express";
 
@@ -32,28 +32,44 @@ app.use(express.json());
 
 // direct request handling
 app.get('/credits', function (req, res) {
-	res.render('credits');
+  res.render('credits');
 });
 
 
 // express routing
-app.use("/owners",  ownerRouter);
-app.use("/",        viewRouter);
+app.use("/owners", ownerRouter);
+app.use("/", viewRouter);
 
 
 // Error Handling Middleware
-app.use(function(err: Error, req: Request, res: Response, next: NextFunction) {
+app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
   console.error(err.stack);
   res.status(500).send(`<html>
   <head>
-    <title>Weiterleitung</title>
-    <meta http-equiv="refresh" content="3;url=/new-page" />
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&display=swap" rel="stylesheet">
+      <style>
+          body {
+              font-family: "Maven Pro", sans-serif;
+              margin: 0;
+              padding: 0;
+              display: flex;          
+              justify-content: center;           
+              align-items: center;           
+              flex-direction: column;
+          }
+      </style>
+      <title>Weiterleitung</title>
+      <meta http-equiv="refresh" content="3;url=/new-page" />
   </head>
+  
   <body>
-    <h1>${err.message}</h1>
-    <p>You will be redirected in 3 seconds.</p>
+      <h1>${err.message}</h1>
+      <p>You will be redirected in 3 seconds.</p>
   </body>
-</html>`);
+  
+  </html;`);
 });
 
 export default app;
