99f9f5c9897aa73a9bfd99c66b9120f5d0590b19
ha-da-ra <gold85600@gmail.com>
2024-05-21 16:20:10 +0200

    add styling to error page

--- src/app.ts
+++ src/app.ts
@@ -41,19 +41,36 @@ app.use("/owners",  ownerRouter);
 app.use("/",        viewRouter);
 
 
+
 // Error Handling Middleware
-app.use(function(err: Error, req: Request, res: Response, next: NextFunction) {
+app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
   console.error(err.stack);
   res.status(500).send(`<html>
   <head>
-    <title>Weiterleitung</title>
-    <meta http-equiv="refresh" content="3;url=/new-page" />
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&display=swap" rel="stylesheet">
+      <style>
+          body {
+              font-family: "Maven Pro", sans-serif;
+              margin: 0;
+              padding: 0;
+              display: flex;          
+              justify-content: center;           
+              align-items: center;           
+              flex-direction: column;
+          }
+      </style>
+      <title>Weiterleitung</title>
+      <meta http-equiv="refresh" content="3;url=/new-page" />
   </head>
+  
   <body>
-    <h1>${err.message}</h1>
-    <p>You will be redirected in 3 seconds.</p>
+      <h1>${err.message}</h1>
+      <p>You will be redirected in 3 seconds.</p>
   </body>
-</html>`);
+  
+  </html;`);
 });
 
 export default app;
