d8b455b93fc567262a86544919aa3f909d6febbe
Thomas <t.dluhosch@gmail.com>
2024-05-23 10:11:15 +0200

    removed dogs

--- src/database/owners/owner-crud.ts
+++ src/database/owners/owner-crud.ts
@@ -1,13 +1,10 @@
 import { Owner } from "./OwnerModel";
 import { IOwner } from "./IOwner";
 
-
-export function addOwner(newOwner: IOwner) {
-   
-}
+export function addOwner(newOwner: IOwner) {}
 
 export function updateOwner(name: string, changedOwner: IOwner) {
-   return Owner.updateOne({ name: name }, changedOwner);
+    return Owner.updateOne({ name: name }, changedOwner);
 }
 
 export function deleteOwner(name: string) {
@@ -27,60 +24,50 @@ export async function createMockOwners() {
             {
                 name: "Jasmine",
                 allergies: "none",
-                gender: "female"
-            }, {
+                gender: "female",
+            },
+            {
                 name: "Paolo",
                 allergies: "fleas",
-                gender: "male"
-            }, {
+                gender: "male",
+            },
+            {
                 name: "Toby",
                 allergies: "none",
-                gender: "male"
-            }
-        ]
-    }
+                gender: "male",
+            },
+        ],
+    };
     return await Owner.create(mockOwners);
 }
 
-
 export async function getAllOwners() {
-    return [{
-        name: "William",
-        number: "+123 456 789",
-        dogs: [
-            {
-                name: "Jasmine",
-                allergies: "none",
-                gender: "female"
-            }, {
-                name: "Paolo",
-                allergies: "fleas",
-                gender: "male"
-            }, {
-                name: "Toby",
-                allergies: "none",
-                gender: "male"
-            }
-        ]
-    },
-    {
-        name: "John",
-        number: "+123 456 789",
-        dogs: [
-            {
-                name: "Jasmine",
-                allergies: "none",
-                gender: "female"
-            }, {
-                name: "Paolo",
-                allergies: "fleas",
-                gender: "male"
-            }, {
-                name: "Toby",
-                allergies: "none",
-                gender: "male"
-            }
-        ]
-    }];
+    return [
+        {
+            name: "William",
+            number: "+123 456 789",
+            dogs: [
+                {
+                    name: "Jasmine",
+                    allergies: "none",
+                    gender: "female",
+                },
+                {
+                    name: "Paolo",
+                    allergies: "fleas",
+                    gender: "male",
+                },
+                {
+                    name: "Toby",
+                    allergies: "none",
+                    gender: "male",
+                },
+            ],
+        },
+        {
+            name: "John",
+            number: "+123 456 789",
+            dogs: [],
+        },
+    ];
 }
-
